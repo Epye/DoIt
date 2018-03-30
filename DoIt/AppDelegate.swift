4776b531f1c120bcebe91d99934337594a951139
Nicolas <nic.quillon@gmail.com>
2018-03-30 09:34:11 +0200

    Initial Commit

---
+++ DoIt/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DoIt
+//
+//  Created by iem on 30/03/2018.
+//  Copyright © 2018 iem. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+
+}
+
