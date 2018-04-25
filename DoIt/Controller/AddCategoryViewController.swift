909312e5d492d13533eb4ea828bfb42f59e462d0
Adeline <adeline-dumas@hotmail.fr>
2018-04-25 14:21:54 +0200

    Modification add/edit categorie

---
+++ DoIt/Controller/AddCategoryViewController.swift
@@ -0,0 +1,76 @@
+//
+//  AddCategoryViewController.swift
+//  DoIt
+//
+//  Created by iem on 25/04/2018.
+//  Copyright © 2018 iem. All rights reserved.
+//
+import Foundation
+import UIKit
+import CoreData
+
+class AddCategoryViewController: UITableViewController, UITextFieldDelegate {
+
+    var delegate : AddCategoryViewControllerDelegate!
+    var categoryToEdit : Categorie!
+    @IBOutlet weak var textFieldSaisieCategory: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let categoryEdit = categoryToEdit {
+            self.navigationItem.title = "Edit Category"
+            textFieldSaisieCategory.text = categoryEdit.nom
+        }
+    }
+    
+    @IBAction func done() {
+        if let category = textFieldSaisieCategory.text {
+            if (categoryToEdit) != nil {
+                categoryToEdit.nom = category
+                delegate.addCategoryViewController(self, didFinishEditingCategory: categoryToEdit)
+            } else {
+                delegate.addCategoryViewController(self, didFinishAddingCategory: category)
+            }
+        }
+    }
+    
+    
+    @IBAction func cancel() {
+        delegate.addCategoryViewControllerDidCancel(self)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        textFieldSaisieCategory.delegate = self
+        self.navigationItem.rightBarButtonItem?.isEnabled = false
+        textFieldSaisieCategory.becomeFirstResponder()
+        if !((textFieldSaisieCategory.text?.isEmpty)!){
+            self.navigationItem.rightBarButtonItem?.isEnabled = true
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let oldString = textField.text {
+            let newString = oldString.replacingCharacters(in: Range(range, in: oldString)!, with: string)
+            if newString.isEmpty{
+                self.navigationItem.rightBarButtonItem?.isEnabled = false
+            }
+            else {
+                self.navigationItem.rightBarButtonItem?.isEnabled = true
+            }
+            return true
+        }
+        return false
+    }
+}
+
+protocol AddCategoryViewControllerDelegate : class {
+    func addCategoryViewControllerDidCancel(_ controller: AddCategoryViewController)
+    func addCategoryViewController(_ controller: AddCategoryViewController, didFinishAddingCategory category: String)
+    func addCategoryViewController(_ controller: AddCategoryViewController, didFinishEditingCategory category: Categorie)
+    
+}
--- DoIt/Controller/CatagoryViewController.swift
+++ DoIt/Controller/CatagoryViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class CatagoryViewController: UITableViewController {
 
@@ -22,13 +23,8 @@ class CatagoryViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return dataManager.getItems().count
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -39,10 +35,47 @@ class CatagoryViewController: UITableViewController {
             }
         }
         else if segue.identifier == "addCategory" {
-
+            if let navVC = segue.destination as? UINavigationController, let destVC = navVC.topViewController as? AddCategoryViewController {
+                destVC.delegate = self
+            }
         }
         else if segue.identifier == "editCategory" {
-
+            if let navVC = segue.destination as? UINavigationController, let destVC = navVC.topViewController as? AddCategoryViewController {
+                let cell = sender as! UITableViewCell
+                destVC.delegate = self
+                destVC.categoryToEdit = dataManager.getItem(index:(tableView.indexPath(for: cell)?.row)!)
+            }
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for : indexPath)
+        let item = dataManager.getItem(index: indexPath.row)
+        cell.textLabel?.text = item.nom
+        return cell
+    }
+    
+    
+}
+
+extension CatagoryViewController : AddCategoryViewControllerDelegate {
+    func addCategoryViewControllerDidCancel(_ controller: AddCategoryViewController){
+        controller.dismiss(animated: true)
+    }
+    
+    func addCategoryViewController(_ controller: AddCategoryViewController, didFinishAddingCategory category: String){
+        controller.dismiss(animated: true)
+        let categorie = Categorie(context: self.dataManager.persistentContainer.viewContext)
+        categorie.nom = category
+        self.dataManager.addItem(item: categorie)
+        tableView.reloadData()
+        
+    }
+    
+    func addCategoryViewController(_ controller: AddCategoryViewController, didFinishEditingCategory category: Categorie){
+        controller.dismiss(animated: true)
+        let index = IndexPath(item : dataManager.cachedItems.index(where:{ $0 === category })!, section : 0)
+        tableView.reloadRows(at: [index] , with: UITableViewRowAnimation.none)
+        tableView.reloadData()
     }
 }
